42371e1c43ee3362b2687342891013986e2e5256
viresh18 <viresh.renukoot@gmail.com>
2018-09-23 16:04:57 +0530

    Added permissions and authentication to our UserProfileViewSet.

--- src/bookstore_project/bookstore_api/views.py
+++ src/bookstore_project/bookstore_api/views.py
@@ -4,12 +4,16 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from . import serializers
 from . import models
+from . import permissions
 
 # Create your views here.
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handles creating, updating and deleting profiles."""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
