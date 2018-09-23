cabacaadee01e900396b2fd34195014c016d4258
viresh18 <viresh.renukoot@gmail.com>
2018-09-23 15:46:26 +0530

    Added profile listing and creating feature

--- src/bookstore_project/bookstore_api/views.py
+++ src/bookstore_project/bookstore_api/views.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from rest_framework import viewsets
+
+from . import serializers
+from . import models
+
 # Create your views here.
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating, updating and deleting profiles."""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
