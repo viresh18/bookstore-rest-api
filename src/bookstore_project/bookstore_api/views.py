2402d5831a415dda7af9aca5855a0cf827bdb37c
viresh18 <viresh.renukoot@gmail.com>
2018-09-23 17:04:52 +0530

    Added search function to bookstore user profiles API

--- src/bookstore_project/bookstore_api/views.py
+++ src/bookstore_project/bookstore_api/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from . import serializers
 from . import models
@@ -17,3 +18,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
